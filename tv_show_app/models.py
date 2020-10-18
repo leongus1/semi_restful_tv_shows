555b670370b0145dc5d223c51354b7e8dec8be86
leongus1 <leongus1@gmail.com>
2020-10-18 16:57:30 -0700

    fixed the show title issue in Edit

--- tv_show_app/models.py
+++ tv_show_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Shows(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
     network = models.CharField(max_length=55)
     release_date = models.DateField()
     desc = models.TextField()
