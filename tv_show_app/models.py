d6567a939b6d05462779ba22be0698c9043502c1
leongus1 <leongus1@gmail.com>
2020-10-19 20:36:25 -0700

    added duplicate title validation and future release date validation

--- tv_show_app/models.py
+++ tv_show_app/models.py
@@ -1,10 +1,37 @@
+from __future__ import unicode_literals
 from django.db import models
+import datetime
+
 
 # Create your models here.
+
+class ShowManager(models.Manager):
+    def validator(self, postData):
+        errors={}
+        if postData['create_or_update']=="Create":
+            if Shows.objects.filter(title=postData['title']).count() > 0:
+                errors['title_duplication'] = "There is already a show with this name in the database"
+        
+        if len (postData['title'])<2:
+            errors['name']="Title must be at least 2 characters long."
+        if len(postData['network'])<3:
+            errors['network']="Network must be at least 3 characters long."
+        if len(postData['description']) < 10 and len(postData['description'])>0:
+            errors['description']="Description must be at least 10 characters long."
+        if postData['release']>str(datetime.date.today()):
+            errors['release']="Release date must not be in the future."
+        print ('postdata: ', postData['release'], 'today: ', datetime.date.today())
+            
+        return errors
+        
+        
 class Shows(models.Model):
     title = models.CharField(max_length=100)
     network = models.CharField(max_length=55)
     release_date = models.DateField()
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
+    
+    
--- tv_show_app/views.py
+++ tv_show_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
-from tv_show_app.models import Shows
+from tv_show_app.models import Shows, ShowManager
 import datetime
+from django.contrib import messages
 
 # Create your views here.
 
@@ -36,27 +37,44 @@ def enter_show(request):
     return render(request, 'add_edit_show.html', context)
 
 def add_new(request):
-    Shows.objects.create(title = request.POST['title'], network = request.POST['network'], release_date = request.POST['release'], desc = request.POST['description'] )
-    show_id = Shows.objects.get(title = request.POST['title'])
-    show_id = show_id.id
-    return redirect (f'/shows/{show_id}')
+    errors = Shows.objects.validator(request.POST)
+    if len(errors)>0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new/')
+    else: 
+        Shows.objects.create(title = request.POST['title'], network = request.POST['network'], release_date = request.POST['release'], desc = request.POST['description'] )
+        show_id = Shows.objects.get(title = request.POST['title'])
+        show_id = show_id.id
+        messages.success(request, "Show successfully created!")
+        return redirect (f'/shows/{show_id}')
 
 def destroy(request, show_id):
     Shows.objects.get(id=show_id).delete()
     return redirect('/')
 
 def update_show(request, show_id):
-    this_show = Shows.objects.get(id=show_id)
-    this_show.title=request.POST['title']
-    this_show.network = request.POST['network']
-    if not request.POST['release']=="":
-        this_show.release_date=request.POST['release']
-    this_show.desc = request.POST['description']
-    this_show.modified_at = datetime.datetime.now()
-    this_show.save()
-    this_path = f"/shows/{show_id}"
+    errors = Shows.objects.validator(request.POST)
+    if not Shows.objects.get(id=show_id).title == request.POST['title']:
+        if Shows.objects.filter(title=request.POST['title']).count()>0:
+                errors['title_duplication'] = "There is already a show with this name in the database"
+        
     
-    return redirect(this_path)
+    if len(errors)>0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{show_id}/edit/')
+    else: 
+        this_show = Shows.objects.get(id=show_id)
+        this_show.title=request.POST['title']
+        this_show.network = request.POST['network']
+        if not request.POST['release']=="":
+            this_show.release_date=request.POST['release']
+            
+        this_show.desc = request.POST['description']
+        this_show.save()
+        messages.success(request, "Show successfully updated!")
+        return redirect(f"/shows/{show_id}")
 
 def default(request):
     return redirect('/shows')
